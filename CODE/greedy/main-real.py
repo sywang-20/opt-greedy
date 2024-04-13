dbd27ca4449eb1c27021f9b350f022e37facc42d
sywang-20 <sywang410@163.com>
2024-04-13 15:44:48 +0800

    update main for new greedy

--- CODE/greedy/main-real.py
+++ CODE/greedy/main-real.py
@@ -46,15 +46,13 @@ if __name__ == '__main__':
     --outdir simulation结果的输出目录
     """
     parser = argparse.ArgumentParser(usage="it's usage tip.", description="help info.")
-    parser.add_argument("--max_sensor", type=int, default=5, help="the max_sensoration number")  #迭代次数，sensor number的upper bound
+    parser.add_argument("--max_sensor", type=int, default=5, help="upper bound for sensor number")  #迭代次数，sensor number的upper bound
     parser.add_argument("--num_of_individual", type=int, default=10, help="the number of reserved plans in each step")
     parser.add_argument("--new_plans", type=int, default=20, help="the number of new plans generated from one sensor")
     parser.add_argument("--datadir", type=str, default="../../DATA/real_life_case_network/data/")  # 对应网络数据的保存目录
     parser.add_argument("--outdir", type=str,
-                        default="../../TESTOUTPUT/local_search/greedy_evo/")  # 对应输出结果的保存目录
-    # parser.add_argument("--coverage", type=int, default=500, help="maximum coverage increment")
-    # parser.add_argument("--searchcost", type=float, default=0.01, help="maximum search cost")
-    # parser.add_argument("--plans_num_per", type=int, default=20, help="the number of reserved new plans generated from one sensor")
+                        default="../../TESTOUTPUT/real_case/greedy_new/")  # 对应输出结果的保存目录
+
 
     args = parser.parse_args()
 
@@ -64,22 +62,12 @@ if __name__ == '__main__':
     parent_dir = args.datadir
     num_of_individuals = args.num_of_individual
     new_plans_num = args.new_plans
-    #plans_num_per=args.plans_num_per
-    # coverage_increment_max = args.coverage
-    # search_cost_increment_max = args.searchcost
-    # sensor_upbound = args.upbound
-    # sensor_lowbound = args.lowbound
-
-    # upstream_set, upstream_arr, relabeled_G, node_num, conn_dict=pre_read_syn()
 
 
     upstream_set, upstream_arr, relabeled_G, node_num, conn_dict = pre_read_real(parent_dir)
 
 
-    start=time.time()
-    #通过循环进行多次计算
-    #for i in range(10):
-    #fig_path = os.path.join(output_dir,'max_sensor_' + str(max_sensor) +'_coverage_' + str(coverage_increment_max) +'_search_cost_' + str(search_cost_increment_max) +'_Lmax_' + str(num_of_individuals) + '_new_plans_' + str(new_plans_num) + '/')
+    # start=time.time()
     fig_path = os.path.join(output_dir, 'max_sensor_' + str(max_sensor) + '_Lmax_' + str(num_of_individuals) + '_new_plans_'+ str(new_plans_num) + '/')
 
     # 根据输出数据目录是否存在，创建目录
@@ -96,7 +84,7 @@ if __name__ == '__main__':
     priority = np.ones(node_num)
 
     # Describes important constraints in the simulation implementation.构建这么一个多目标优化的问题，用于后续使用evo进行求解
-    problem = Problem(objectives=[objective.coverage, objective.new_search_cost_by_topology_2],
+    problem = Problem(objectives=[objective.coverage, objective.search_cost],
                       constraint=[constraint.sensor_num],
                       # 目标函数
                       node_num=node_num, upstream_arr=upstream_arr,  # 限制条件以及一些需要的数据
@@ -106,13 +94,13 @@ if __name__ == '__main__':
 
     # controls the max_sensorative process of NSGA-II. evolution函数对上面构建的problem进行求解-->需要修改算法的话，应该主要修改evolution里的东西
     evo = Evolution(problem, max_sensor=max_sensor, num_of_individuals=num_of_individuals,  #每个max_sensoration保留多少plan
-                    new_plans_num=new_plans_num,  #每一个plan0生成多少个新plan   ,batch_size=10
-                    #coverage_increment_max=coverage_increment_max, search_cost_increment_max=search_cost_increment_max,  #plans_num_per=plans_num_per,
+                    new_plans_num=new_plans_num,  #每一个plan0生成多少个新plan   ,batch_size=1
                     fig_path=fig_path)
 
+    print("start")
+    start = time.time()
     evo.evolve()
-
     end=time.time()
+
     with open(os.path.join(fig_path, "time.txt"), "w") as tf:
         tf.write(str(end-start))
-    #print("execution time: "+ str(end-start))
