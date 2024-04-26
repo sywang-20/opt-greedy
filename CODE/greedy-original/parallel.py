4601e5143177e279840e2aca5230c0484fe4bb43
sywang-20 <sywang410@163.com>
2024-04-26 10:08:31 +0800

    get new results-add new exp

--- CODE/greedy-original/parallel.py
+++ CODE/greedy-original/parallel.py
@@ -15,7 +15,8 @@ def run_case_size(size):
 if __name__ == '__main__':
     # size = list(range(100, 2100, 100))
     # size.extend([2500, 3000])
-    size=[100,500,1000,1500,2000,2500,3000]
+    #size=[100,500,1000,1500,2000,2500,3000]
+    size = [200, 300, 400, 600, 700, 800, 900]
     print(size)
     cpu_count = os.cpu_count()
     #t = [(onesize) for onesize in size]
