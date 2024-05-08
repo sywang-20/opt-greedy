e9e93193a6e4f37d89677f474bb7ddba67c2e8c4
sywang-20 <sywang410@163.com>
2024-05-08 15:51:24 +0800

    #12 use the former code, simplified code is wrong

--- CODE/greedy-original/evolution.py
+++ CODE/greedy-original/evolution.py
@@ -15,9 +15,6 @@ import pickle
 
 
 class Evolution:
-
-
-
     def __init__(self, problem, max_sensor=5, num_of_individuals=100,
                  fig_path='./output/',sol={}, node_num=10):
         print('init Evolution')
@@ -33,9 +30,59 @@ class Evolution:
         self.initial_individual = None
         self.sol=sol
         self.node_num=node_num
-        #self.num_of_solutions = num_of_solutions
-        #self.coverage_increment_max=coverage_increment_max   # coverage的stopping criterion
-        #self.search_cost_increment_max=search_cost_increment_max  # search cost的stopping criterion
+
+    def evolve(self):
+        fig_path = self.fig_path
+
+        if not os.path.exists(fig_path):
+            os.makedirs(fig_path)
+        self.population = self.utils.create_initial_population()
+
+        solutions_dict = {}
+
+        i = 0
+        print('sensor number upper bound: '+str(self.max_sensor))
+        print('reserved number of plans at each iteration: '+str(self.num_of_individuals))
+
+        while True:
+            if i >= self.max_sensor:
+                break
+            else:
+                neighbors = Population()
+                for j in self.population:
+                    j_neighbor = self.utils.problem.create_individual_one_more_sensor_all(j)
+                    neighbors.extend(j_neighbor)
+
+                neighbors=self.utils.duplication_elimination(neighbors)
+
+                #------fast non-dominated sorting，进行Lmax个解的选择----------
+                self.utils.fast_nondominated_sort(neighbors)
+                new_population = Population()
+                front_num = 0
+
+                # 前一步选取出来的solution，加上目前该front的数，比想要的solution总数小，把这个front的解全部加进去
+                while len(new_population) + len(neighbors.fronts[front_num]) <= self.num_of_individuals:
+                    self.utils.calculate_crowding_distance(neighbors.fronts[front_num])
+                    new_population.extend(neighbors.fronts[front_num])  # 把这个front全部加进去
+                    front_num += 1  # 更新front
+                    print(front_num)
+
+                print('final front num', front_num)  # 第一个不满足while判断的front，可能只有部分可以加进去
+                self.utils.calculate_crowding_distance(neighbors.fronts[front_num])  # 对这个front的crowding distance进行计算
+                neighbors.fronts[front_num].sort(key=lambda individual: individual.crowding_distance,reverse=True)  # 计算了crowding distance后，对其进行排序
+                new_population.extend(neighbors.fronts[front_num][0:self.num_of_individuals - len(new_population)])  # 把符合数量要求的solution加进去
+
+                self.population = new_population
+
+                solutions_dict[i] = [[i.constraint[0], i.objectives[0], i.objectives[1], i.positive_nodes] for i in
+                                    self.population]
+
+            i=i+1
+
+        with open(self.fig_path + '/solution.pkl', 'wb') as f:
+            pickle.dump(solutions_dict, f)
+
+
 
     # calculate the objective function for one specific placement
     def evolve_new_syn(self):
@@ -66,50 +113,5 @@ class Evolution:
 
             solution_obj[x] = temp
             print(solution_obj)
-
-
-    def evolve(self):
-        fig_path = self.fig_path
-
-        if not os.path.exists(fig_path):
-            os.makedirs(fig_path)
-        self.population = self.utils.create_initial_population()
-
-        solutions_dict = {}
-
-        i = 0
-        print('sensor number upper bound: '+str(self.max_sensor))
-        print('reserved number of plans at each iteration: '+str(self.num_of_individuals))
-
-        while True:
-            if i >= self.max_sensor:
-                break
-            else:
-                neighbors = Population()
-                for j in self.population:
-                    j_neighbor = self.utils.problem.create_individual_one_more_sensor_all(j)
-                    neighbors.extend(j_neighbor)
-
-                neighbors=self.utils.duplication_elimination(neighbors)
-
-                #------fast non-dominated sorting，进行Lmax个解的选择----------
-                self.utils.fast_nondominated_sort(neighbors)
-                for front in neighbors.fronts:
-                    self.utils.calculate_crowding_distance(front)
-                # 按照domination_count从小到大，crowding_distance从大到小进行排序 (domination_count表示一个solution被其他solution所支配的次数)
-                neighbors.population.sort(key=lambda individual: (individual.domination_count, -individual.crowding_distance))
-                self.population = neighbors.population[0:self.num_of_individuals]
-
-                solutions_dict[i] = [[i.constraint[0], i.objectives[0], i.objectives[1], i.positive_nodes] for i in
-                                    self.population]
-
-            i=i+1
-
-        with open(self.fig_path + '/solution.pkl', 'wb') as f:
-            pickle.dump(solutions_dict, f)
-
-
-
-
 
 
